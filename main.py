a312847bd93991ba4948b3fb69e8a6db55c1779a
Abirokaya <rokayaabinash9@gmail.com>
2026-05-28 17:55:36 +0545

    add new fronend url

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app=FastAPI()
 Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",'https://ecommerce-api-kd57.onrender.com'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
